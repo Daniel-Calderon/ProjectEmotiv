045ea000e2a6ffd724241fe7d12bce393ec5bbf3
brandanlockwood <blockwood@csumb.edu>
2016-03-08 17:39:29 -0800

    randoColor function working

--- shapeRandom.py
+++ shapeRandom.py
@@ -1,14 +1,17 @@
 from visual import *
 import random
 def randoColor(shape):
-    randomNumber1 = random.randrange(0,256)
-    randomNumber2 = random.randrange(0,256)
-    randomNumber3 = random.randrange(0,256)
+    randomNumber1 = random.randrange(0,2)
+    randomNumber2 = random.randrange(0,2)
+    randomNumber3 = random.randrange(0,2)
+    if(randomNumber3==0 and randomNumber2==0 and randomNumber1==0):
+        print shape.color
+        randonNumber1=1
+        randomNumber3=1
     shape.color= (randomNumber1,randomNumber2,randomNumber3)
     return shape
 def randomShape(shape):
-    randomNumber = random.randrange(1,9)
-    
+    randomNumber = random.randrange(1,9)   
     if randomNumber == 1:
         shape = box(pos = (0,0,0),axis = (0,0,1),color =(0,244,0),length =1,width =1, height =1)
     elif randomNumber == 2:
@@ -25,7 +28,6 @@ def randomShape(shape):
         shape = ring(pos=(0,0,0), axis=(0,0,1),color =(0,244,0), radius=0.5, thickness=0.1)
     elif randomNumber == 8:
         shape = sphere(pos=(0,0,0),color =(0,244,0), radius=0.5)
-    print randomNumber
     return shape
     rate(5)
 shape = box(pos = (0,0,0),axis = (0,0,1),color = (0,244,0),length =1,width =1, height =1)
